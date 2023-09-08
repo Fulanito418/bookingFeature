7237765f510b10a84228e9f683a07ef1233c16cb
Alex Perez <63934973+Fulanito418@users.noreply.github.com>
2023-09-08 17:04:40 -0400

    Update getOfficerndMeetingRoomsController.js

--- getOfficerndMeetingRoomsController.js
+++ getOfficerndMeetingRoomsController.js
@@ -1,11 +1,7 @@
-/* eslint-disable no-console */
 import InternalServerError from "../../errors/internalServerError.js";
 import { STATUS_CODE } from "../../types/Contants.js";
 import getOfficerndAccessTokenController from "../main/getOfficerndAccessTokenController.js";
 import axios from "axios";
-//Using Axios instead >> import fetch from 'node-fetch';
-
-
 
 async function getMeetingRooms() {
     // Grab meeting rooms from 'resources' endpoint:
@@ -15,11 +11,6 @@ async function getMeetingRooms() {
   const token = await getOfficerndAccessTokenController();
   let meetingRooms = null;
 
-
-    
-//   var data = qs.stringify({
-//     'authorization': `Bearer ${token}` 
-//   });
   var config = {
     method: 'GET',
     url: 'https://app.officernd.com/api/v1/organizations/cohatch/resources?type=meeting_room&$populate=office.physicalAddress.city,office.physicalAddress.state,rate.price',
@@ -34,10 +25,6 @@ async function getMeetingRooms() {
   await axios(config)
   .then(function (response) {
     meetingRooms = response.data
-    //Testing
-    // console.log(JSON.stringify(response.data));
-    // console.log(response.data);
-    // console.log(JSON.stringify(response.data[3].image));
   })
   .catch(function (error) {
     // eslint-disable-next-line no-console
@@ -45,19 +32,12 @@ async function getMeetingRooms() {
   });
   return meetingRooms
 }
-// getMeetingRooms();
-
-
-
-  
-
 
 async function getOfficerndMeetingRoomsController(req, res, next) {
 
   const meetingRooms = await getMeetingRooms();
-  
-  try {
 
+  try {
     res.status(STATUS_CODE.SUCCESS).json({
       status: "success",
       statusCode: STATUS_CODE.SUCCESS,
